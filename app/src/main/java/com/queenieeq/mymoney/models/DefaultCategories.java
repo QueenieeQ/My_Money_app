2b14e0cee50c67ba538887075d5810244df9604c
QueenieeQ <xuanquyhnvn@gmail.com>
2022-06-14 22:24:56 +0700

    Change same value in base activity

--- app/src/main/java/com/queenieeq/mymoney/models/DefaultCategories.java
+++ app/src/main/java/com/queenieeq/mymoney/models/DefaultCategories.java
@@ -6,8 +6,8 @@ import com.queenieeq.mymoney.R;
 
 public  class DefaultCategories {
     private static Category[] categories = new Category[]{
-            new Category(":sarathi", "Sarathi", R.drawable.category_transport, Color.parseColor("#d32f2f")),
-            new Category(":parivahan", "Parivahan", R.drawable.category_kids, Color.parseColor("#c2185b")),
+            new Category(":food", "Food", R.drawable.category_food, Color.parseColor("#d32f2f")),
+            new Category(":clothing", "Clothing", R.drawable.category_clothing, Color.parseColor("#c2185b")),
             new Category(":work", "Work", R.drawable.category_briefcase, Color.parseColor("#f57c00")),
             new Category(":others", "Others", R.drawable.category_gift, Color.parseColor("#455a64")),
     };
